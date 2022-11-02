16f474e8c6761a6b3a7888687ac0406146530efe
bala8825 <117162208+bala8825@users.noreply.github.com>
2022-11-02 11:38:59 +0530

    Update dbt1.sql

--- models/example/dbt1.sql
+++ models/example/dbt1.sql
@@ -1,1 +1,13 @@
+dbttrans1 as (select * from test12)
 
+with
+
+bqviewsql as (select 
+id as student_id,
+name as Name,
+email as student_email_id,
+age as Age
+from dbttrans1
+)
+
+select * from bqviewsql
